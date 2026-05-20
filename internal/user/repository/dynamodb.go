d32b9cb1e10748e51ad7866b87b6240a7c4bc490
JLUGUOymUEC <guoymjlu@gmail.com>
2026-05-20 17:05:21 +0800

    commit_20250520

--- internal/user/repository/dynamodb.go
+++ internal/user/repository/dynamodb.go
@@ -2,17 +2,64 @@ package repository
 
 import (
 	"context"
+	"fmt"
+	
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
+	"gin-demo/internal/user/model"
 )
 
 type DynamoUserRepository struct {
-	client *dynamodb.Client
+	client    *dynamodb.Client
 	tableName string
 }
 
-func NewDynamoUserRepository(client *dynamodb.Client, tableName string) *DynamoUserRepository {
-	return &DynamoUserRepository{
-		client: client,
-		tableName: tableName,
+func NewDynamoDBConfig(ctx context.Context) (*dynamodb.Client, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("ap-northeast-1"))
+	if err != nil {
+		return nil, err
 	}
+	dbClient := dynamodb.NewFromConfig(cfg)
+
+	return dbClient, nil
 }
+
+func NewDynamoUserRepository(ctx context.Context) (*DynamoUserRepository, error) {
+	client, err := NewDynamoDBConfig(ctx)
+	if err != nil {
+		err := fmt.Errorf("Failed to load AWS config: " + err.Error())
+		return nil, err
+	}
+	tableName := "Users"
+
+	return &DynamoUserRepository{
+		client:    client,
+		tableName: tableName,
+	}, nil
+}
+
+
+func (repo *DynamoUserRepository)CreateUser(ctx context.Context, user *model.User) error {
+	if err := user.Validate();err != nil{
+		return err
+	}
+	user.BeforeCreate()
+	//将User结构体转换为DynamoDB项
+	item, err := attributevalue.MarshalMap(user)
+	if err != nil {
+		return fmt.Errorf("Failed to marshal user: " + err.Error())	
+	}
+	//将项写入DynamoDB表
+	_, err = repo.client.PutItem(ctx , 
+		&dynamodb.PutItemInput{
+			TableName: &repo.tableName,
+			Item: item,
+		})
+	if err != nil {
+		return fmt.Errorf("Failed to put item: " + err.Error())
+	}
+	return nil
+}
+
+func (repo *DynamoUserRepository) GetUserByID(ctx context
--- internal/user/repository/model.go
+++ internal/user/repository/model.go
@@ -1,5 +1,14 @@
 package repository
 
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/google/uuid"
+)
 
 //dyanamodbav标签用于aws-sdk-go-v2的dynamodbav包进行结构体与DynamoDB项之间的映射
 type User struct {
@@ -7,4 +16,28 @@ type User struct {
 	Username string `dynamodbav:"username"`
 	Password string `dynamodbav:"password"`
 	Email string `dynamodbav:"email"`
+	CreatedAt int64 `dynamodbav:"created_at"`
+	UpdatedAt int64 `dynamodbav:"updated_at"`
+}
+
+
+func (user *User) Validate() error {
+	if user.Username == "" {
+		return fmt.Errorf("Username is required")
+	}
+	if user.Password == "" {
+		return fmt.Errorf("Password is required")
+	}
+	if user.Email == "" {
+		return fmt.Errorf("Email is required")
+	}
+	return nil
+}
+
+func (user *User) BeforeCreate() {
+	//设置CreatedAt和UpdatedAt等
+	now := time.Now().Unix()
+	user.CreatedAt = now
+	user.UpdatedAt = now
+	user.UserID = uuid.New().string()s
 }
